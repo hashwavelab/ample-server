333b56bd171dedc63e4f020ba1331a2d49f8357d
GUA19 <gua@hashwave.io>
2022-06-22 12:40:54 -0400

    remove auth0 at backend

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ const cors = require('cors')
 const MongoClient = require('./utils/mongoClient')
 const Assembler = require('./assembler.js')
 
-// auth0 setup
-const { auth } = require('express-openid-connect')
-require('dotenv').config()
-
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: process.env.BASEURL,
-    clientID: process.env.CLIENTID,
-    issuerBaseURL: process.env.ISSUER,
-};
-
 global.globalMongoClient = new MongoClient();
 setTimeout(() => {
     global.globalAssembler = new Assembler();
@@ -30,13 +17,6 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 app.use(auth(config));
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-    res.redirect('https://ample.hashwave.io')
-});
-app.get('/loginCheck', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? true : false);
-});
 
 app.use('/auth', ethSign);
 app.use('/read', read);
