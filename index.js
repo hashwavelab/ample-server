f29bbeaf3af07c880cc810b5edd59b7dc1717a3b
GUA19 <gua@hashwave.io>
2022-06-23 02:08:59 -0400

    move hardcodes from index.js to .env

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const express = require('express')
 const ethsign = require('./api/ethsign')
 const db = require('./api/db')
 const cors = require('cors')
+require('dotenv').config()
 const MongoClient = require('./utils/mongoClient')
 const Assembler = require('./assembler.js')
 
 const { expressjwt: expressJwt } = require('express-jwt');
 var jwksRsa = require('jwks-rsa');
-// var jwtAuthz = require('express-jwt-authz');
 
 const jwtCheck = expressJwt({
     secret: jwksRsa.expressJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: 'https://dev-pmw60n9g.us.auth0.com/.well-known/jwks.json'
+        jwksUri: process.env.JWKSURI
     }),
-    audience: 'https://ample.hashwave.io/api',
-    issuer: 'https://dev-pmw60n9g.us.auth0.com/',
-    algorithms: ['RS256']
+    audience: process.env.AUDIENCE,
+    issuer: process.env.ISSUER,
+    algorithms: [process.env.ALGORITHM],
 });
 
 global.globalMongoClient = new MongoClient();
@@ -31,15 +31,9 @@ const app = express()
 app.use(jwtCheck);
 
 var corsOptions = {
-    origin: 'https://wavefront.hashwave.io',
+    origin: process.env.ORIGIN,
 }
 app.use(cors(corsOptions));
-
-// var scopeCheck = jwtAuthz(['read:db'], { customUserKey: 'auth' })
-
-// app.get('/test', scopeCheck, async (req, res) => {
-//     res.status(200).send("ok");
-// })
 
 app.use('/ethsign', ethsign);
 app.use('/db', db);
